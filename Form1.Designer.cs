97decf721832c801b0c44f06679edefb441492bd
Luis-Felipe005 <135838158+Luis-Felipe005@users.noreply.github.com>
2023-07-14 17:14:54 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Aula04AppBanco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDepositar = new System.Windows.Forms.Button();
+            this.BtnExtrato = new System.Windows.Forms.Button();
+            this.BtnSalvar = new System.Windows.Forms.Button();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOperacao = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnDepositar
+            // 
+            this.BtnDepositar.Location = new System.Drawing.Point(10, 18);
+            this.BtnDepositar.Name = "BtnDepositar";
+            this.BtnDepositar.Size = new System.Drawing.Size(75, 33);
+            this.BtnDepositar.TabIndex = 0;
+            this.BtnDepositar.Text = "Depositar";
+            this.BtnDepositar.UseVisualStyleBackColor = true;
+            this.BtnDepositar.Click += new System.EventHandler(this.BtnDepositar_Click);
+            // 
+            // BtnExtrato
+            // 
+            this.BtnExtrato.Location = new System.Drawing.Point(286, 164);
+            this.BtnExtrato.Name = "BtnExtrato";
+            this.BtnExtrato.Size = new System.Drawing.Size(75, 23);
+            this.BtnExtrato.TabIndex = 2;
+            this.BtnExtrato.Text = "Ver extrato";
+            this.BtnExtrato.UseVisualStyleBackColor = true;
+            this.BtnExtrato.Click += new System.EventHandler(this.BtnExtrato_Click);
+            // 
+            // BtnSalvar
+            // 
+            this.BtnSalvar.Location = new System.Drawing.Point(16, 79);
+            this.BtnSalvar.Name = "BtnSalvar";
+            this.BtnSalvar.Size = new System.Drawing.Size(179, 29);
+            this.BtnSalvar.TabIndex = 3;
+            this.BtnSalvar.Text = "Salvar";
+            this.BtnSalvar.UseVisualStyleBackColor = true;
+            this.BtnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.Location = new System.Drawing.Point(141, 18);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(73, 33);
+            this.btnSacar.TabIndex = 4;
+            this.btnSacar.Text = "Sacar";
+            this.btnSacar.UseVisualStyleBackColor = true;
+            this.btnSacar.Click += new System.EventHandler(this.btnSacar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblOperacao);
+            this.panel1.Controls.Add(this.txtValor);
+            this.panel1.Controls.Add(this.BtnSalvar);
+            this.panel1.Location = new System.Drawing.Point(10, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(204, 124);
+            this.panel1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Informe o valor:";
+            // 
+            // lblOperacao
+            // 
+            this.lblOperacao.AutoSize = true;
+            this.lblOperacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperacao.Location = new System.Drawing.Point(13, 11);
+            this.lblOperacao.Name = "lblOperacao";
+            this.lblOperacao.Size = new System.Drawing.Size(70, 13);
+            this.lblOperacao.TabIndex = 5;
+            this.lblOperacao.Text = "DEPÓSITO";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(16, 53);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(179, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(145, 181);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(37, 15);
+            this.lblSaldo.TabIndex = 8;
+            this.lblSaldo.Text = "R$ 0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(145, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Seu saldo atual:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(185, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Conradito\'s Bank";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.btnSacar);
+            this.groupBox1.Controls.Add(this.BtnDepositar);
+            this.groupBox1.Location = new System.Drawing.Point(136, 216);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 207);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operações disponíveis";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnExtrato);
+            this.Controls.Add(this.lblSaldo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDepositar;
+        private System.Windows.Forms.Button BtnExtrato;
+        private System.Windows.Forms.Button BtnSalvar;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOperacao;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04AppBanco
+{
+    public partial class Form1 : Form
+    {
+
+
+        //Variáveis globais
+        List<String> extratos = new List<String>();
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            double saldo;
+            double valor;
+            valor = double.Parse( txtValor.Text);
+            saldo = double.Parse(lblSaldo.Text.Replace("R$", "") );
+
+            double soma;
+
+            if( lblOperacao.Text == "SAQUE")
+            {
+
+                soma =  saldo - valor;
+                extratos.Add("SAQUE no valor de R$" + valor);
+            }
+            else
+            {
+
+                 soma = valor + saldo;
+                extratos.Add("DEPÓSITO no valor de R$" + valor);
+
+            }
+
+            
+
+
+            soma = saldo + valor;
+
+            lblSaldo.Text = "R$" + soma;
+
+           
+
+
+
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            BtnDepositar.BackColor = Color.Gainsboro;
+            BtnDepositar.ForeColor = Color.Black;
+            btnSacar.BackColor = Color.LightBlue;
+            btnSacar.ForeColor = Color.DodgerBlue;
+
+
+
+            lblOperacao.Text = "SAQUE";
+
+
+
+        }
+
+        private void BtnDepositar_Click(object sender, EventArgs e)
+        {
+            btnSacar.BackColor =  Color.Gainsboro;
+            btnSacar.ForeColor = Color.Black;
+            BtnDepositar.BackColor = Color.LightBlue;
+            BtnDepositar.ForeColor = Color.DodgerBlue;
+            lblOperacao.Text = "DEPÓSITO";
+
+        }
+
+        private void BtnExtrato_Click(object sender, EventArgs e)
+        {
+
+
+            Form2 telaExtrato = new Form2();
+            telaExtrato.extratos = extratos;
+            telaExtrato.Show();
+
+
+
+        }
+    }
+}
